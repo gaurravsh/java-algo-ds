d700fd87e64769d8431a89b53c46192b55d37d91
gauravsharma <21275839+gaurravsh@users.noreply.github.com>
2021-02-27 23:39:02 +0530

    GC learning added

--- src/com/gshar/dsalgo/memorymgmt/UseMXBean.java
+++ src/com/gshar/dsalgo/memorymgmt/UseMXBean.java
@@ -2,6 +2,7 @@ package com.gshar.dsalgo.memorymgmt;
 
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UseMXBean {
@@ -13,14 +14,35 @@ public class UseMXBean {
         for(GarbageCollectorMXBean gcBean : gcBeansList){
             System.out.println("Bean Name : "+gcBean.getName());
             System.out.println("Number of Collections : "+gcBean.getCollectionCount());
-            System.out.println("Collection Time : "+gcBean.getCollectionTime());
+            System.out.println("Collection Time : "+gcBean.getCollectionTime()+" ms");
+            System.out.println("Is Valid? : "+gcBean.isValid());
 
             for (String poolname : gcBean.getMemoryPoolNames()) {
                 System.out.println("Poolname : " + poolname);
             }
             System.out.println();
+        }
 
+        // I tried creating an increasing array list to test if GC is called.
+        // Throws out of memory exception for : long size = 1000_000_000;
+        // I did not bother to tune the heap size with VM arguments
+        // Works well for long size = 100_000_000;
+        long size = 100_000_000;
+        List<Integer> list = new ArrayList<>();
+        for(long i=0;i<size;i++){
+            list.add(0);
+        }
 
+        for(GarbageCollectorMXBean gcBean : gcBeansList){
+            System.out.println("Bean Name : "+gcBean.getName());
+            System.out.println("Number of Collections : "+gcBean.getCollectionCount());
+            System.out.println("Collection Time : "+gcBean.getCollectionTime()+" ms");
+            System.out.println("Is Valid? : "+gcBean.isValid());
+
+            for (String poolname : gcBean.getMemoryPoolNames()) {
+                System.out.println("Poolname : " + poolname);
+            }
+            System.out.println();
         }
     }
 
